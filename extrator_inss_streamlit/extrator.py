7a1d90c2d0e4fb6700a47a791d73369f382c16f5
Samueldasilvacarapina <samuel.carapina@escola.pr.gov.br>
2025-04-30 18:32:48 -0300

    Update extrator.py

--- extrator_inss_streamlit/extrator.py
+++ extrator_inss_streamlit/extrator.py
@@ -1,13 +1,9 @@
-
 import pdfplumber
 import re
 
 rubrica_rmc = '217'
 rubrica_rcc = '268'
 rubricas_sindicato = ['SINDICATO', 'CONTRIB']
-rubricas_descontos = ['201', '216', '104', '207', '218']
-rubrica_salario_bruto = '101'
-indenizacao_fixa = 10000.00
 
 def formatar_valor(valor_str):
     return float(valor_str.replace('.', '').replace(',', '.'))
@@ -36,26 +32,12 @@ def processar_pdf(caminho_pdf):
                 data = extrair_competencia(bloco)
                 if not data:
                     continue
-
                 if data not in dados:
-                    dados[data] = {
-                        'RMC': 0.0,
-                        'RCC': 0.0,
-                        'SINDICATO': 0.0,
-                        'SALARIO_BRUTO': 0.0,
-                        'DESCONTOS': 0.0,
-                        'LIQUIDO': 0.0
-                    }
+                    dados[data] = {'RMC': 0.0, 'RCC': 0.0, 'SINDICATO': 0.0}
 
                 dados[data]['RMC'] += somar_rubrica(bloco, rubrica_rmc)
                 dados[data]['RCC'] += somar_rubrica(bloco, rubrica_rcc)
                 dados[data]['SINDICATO'] += somar_texto(bloco, rubricas_sindicato)
-                dados[data]['SALARIO_BRUTO'] += somar_rubrica(bloco, rubrica_salario_bruto)
-                dados[data]['DESCONTOS'] += sum(somar_rubrica(bloco, r) for r in rubricas_descontos)
 
-    for data in dados:
-        bruto = dados[data]['SALARIO_BRUTO']
-        descontos = dados[data]['DESCONTOS']
-        dados[data]['LIQUIDO'] = bruto - descontos
-
-    return dados
+    # ordenar da mais antiga pra mais recente
+    return dict(sorted(dados.items(), key=lambda x: tuple(map(int, x[0].split("/")[::-1]))))
